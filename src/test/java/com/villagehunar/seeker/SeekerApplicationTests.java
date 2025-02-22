52574c160797b02dc67bba801e4c29950462661e
MokshChoudhary <moksh.choudhary.2@gmail.com>
2025-02-22 17:49:15 +0530

    adding init spring project

---
+++ src/test/java/com/villagehunar/seeker/SeekerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.villagehunar.seeker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SeekerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
